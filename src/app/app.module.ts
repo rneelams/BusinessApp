0b17a9834ca19bd36f821e92c4ed265943b470fe
Neelamsetti <ramesh.neelamsetti@capgemini.com>
2019-08-06 21:12:45 +0530

    Modified Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
+import { CartService } from "./cart.service";
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent,
+    TopBarComponent,
+    ProductAlertsComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
