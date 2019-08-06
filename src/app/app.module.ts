3c9dcf7900591031aa7fa3e0bd0978e37d8d9d4d
Neelamsetti <ramesh.neelamsetti@capgemini.com>
2019-08-06 22:36:07 +0530

    Modified routing links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { TopBarComponent } from "./top-bar/top-bar.component";
-import { ProductListComponent } from "./product-list/product-list.component";
-import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
-import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { CartComponent } from "./cart/cart.component";
-import { ShippingComponent } from "./shipping/shipping.component";
-import { CartService } from "./cart.service";
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { CartService } from './cart.service';
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './loading/loading.component';
 
@@ -20,14 +20,16 @@ import { LoadingComponent } from './loading/loading.component';
     LoadingComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductAlertsComponent,
+    ProductListComponent,
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [CartService],
   bootstrap: [AppComponent]
